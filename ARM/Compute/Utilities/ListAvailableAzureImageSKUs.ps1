d86b68d9851c83c9337f4662b62a9a91dee59424
Will St. Germain <willst@msn.com>
2018-03-20 15:22:52 -0600

    updated and added a couple of utilities.

--- ARM/Compute/Utilities/ListAvailableAzureImageSKUs.ps1
+++ ARM/Compute/Utilities/ListAvailableAzureImageSKUs.ps1
@@ -1,7 +1,17 @@
-﻿$location="usgovtexas"
-#$location="westcentralus"
-$imageName="magImagesAug2017"
-#$imageName="macImages"
+﻿
+$Environment = 'AzureCloud'
+#$Environment = 'AzureUSGovernment'
+
+Login-AzureRmAccount -Environment $Environment
+$subscription = Get-AzureRmSubscription |  Out-GridView -PassThru
+Set-AzureRmContext -SubscriptionId $subscription.Id
+Write-Host "Successfully logged in to Azure." -ForegroundColor Green 
+
+
+#$location="usgovtexas"
+$location="westcentralus"
+$pathName = 'c:\temp\'
+$imageName="macImages031318"
 $Publishers=Get-AzureRmVMImagePublisher -location $Location
 foreach($publisher in $Publishers.PublisherName){
 
@@ -14,6 +24,6 @@ $SKUs=Get-AzureRmVMImageSku -Location $Location -Offer $offer -PublisherName $pu
 foreach($SKU in $SKUs.skus){
 $Images=Get-AzureRmVMImage -Location $location -offer $offer -PublisherName $publisher -skus $Sku 
 
-$images|Export-Csv -Append -Path c:\temp\azure\$imageName.csv
+$images|Export-Csv -Append -Path $pathName+$imageName.csv
 
 }}}
